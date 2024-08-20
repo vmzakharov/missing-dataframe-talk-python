77f9cc4bf2de756a89b814e108a22ee10511c9e4
vmzakharov <31494981+vmzakharov@users.noreply.github.com>
2024-08-20 16:13:30 -0400

    uses time.monotonic()

--- 1brc/pandasdf_1brc.py
+++ 1brc/pandasdf_1brc.py
@@ -1,28 +1,30 @@
 import pandas as pd
 import time
 
+MEASUREMENT_FILE = "D:\\projects\\1brc\\measurements_10.txt"
+
 if __name__ == "__main__":
-    start_time = time.time()
+    start_time = time.monotonic()
 
     df = pd.read_csv(
-        "D:\\projects\\1brc\\" + "measurements_10.txt",
+        MEASUREMENT_FILE,
         sep=";",
         header=None,
-        names=["station", "temperature"],
+        names=["Station", "Temperature"],
         engine='pyarrow'
     )
 
-    load_time = time.time()
+    load_time = time.monotonic()
 
     print(f"Loaded: {load_time - start_time:.2f}s")
 
-    df = df.groupby("station").agg(["min", "max", "mean"])
+    df = df.groupby("Station").agg(["min", "mean", "max"])
+    print(df)
     df.columns = df.columns.droplevel()
 
-    df = df.sort_values("station")
+    df = df.sort_values("Station")
 
-    end_time = time.time()
-
+    end_time = time.monotonic()
     print(f"Done: {end_time - start_time:.2f}s")
 
     for index, row in df.iterrows():
