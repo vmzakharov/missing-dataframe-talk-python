9e7c1816d96bae3fadb0f8fa6e4c9c88a611dd2e
vmzakharov <31494981+vmzakharov@users.noreply.github.com>
2024-08-21 23:28:23 -0400

    code clean up, adds sample data

--- 1brc/pandasdf_1brc.py
+++ 1brc/pandasdf_1brc.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import time
 
-MEASUREMENT_FILE = "D:\\projects\\1brc\\measurements_10.txt"
+FILE = "measurements_10.txt"
+
+def print_time(message, begin_time, end_time):
+    delta_ms = int((end_time - begin_time) * 1000)
+    print(f"{message}: {delta_ms} ms")
+
 
 if __name__ == "__main__":
     start_time = time.monotonic()
 
     df = pd.read_csv(
-        MEASUREMENT_FILE,
+        FILE,
         sep=";",
         header=None,
         names=["Station", "Temperature"],
@@ -16,16 +21,17 @@ if __name__ == "__main__":
 
     load_time = time.monotonic()
 
-    print(f"Loaded: {load_time - start_time:.2f}s")
+    print_time("Loaded", start_time, load_time)
 
     df = df.groupby("Station").agg(["min", "mean", "max"])
-    print(df)
+
     df.columns = df.columns.droplevel()
 
     df = df.sort_values("Station")
 
     end_time = time.monotonic()
-    print(f"Done: {end_time - start_time:.2f}s")
+
+    print_time("Aggregated", load_time, end_time)
 
     for index, row in df.iterrows():
         print(f"{index}={row['min']:0.1f}/{row['mean']:0.1f}/{row['max']:0.1f}")
--- 1brc/pyplain_1brc.py
+++ 1brc/pyplain_1brc.py
@@ -1,6 +1,6 @@
 import time
 
-FILE = "D:\\projects\\1brc\\measurements_100MM.txt"
+FILE = "measurements_10.txt"
 MIN = 0
 MAX = 1
 SUM = 2
@@ -27,17 +27,7 @@ if __name__ == "__main__":
                 stats_by_station[station] = (temperature, temperature, temperature, 1)
 
     end_time = time.monotonic()
-    print(f"Done: {end_time - start_time:.2f}s")
+    print(f"Done: {end_time - start_time:.3f}s")
 
     for station, stats in sorted(stats_by_station.items()):
         print(f"{station}={stats[MIN]:0.1f}/{stats[SUM]/stats[COUNT]:0.1f}/{stats[MAX]:0.1f}")
-
-
-# def print_stats(stats):
-#     formatted = ", ".join(
-#         [
-#             f"{k}={v['min']}/{v['sum']/v['count']:.1f}/{v['max']}"
-#             for k, v in sorted(stats.items(), key=lambda x: x[0])
-#         ]
-#     )
-#     print("{" + formatted + "}")
