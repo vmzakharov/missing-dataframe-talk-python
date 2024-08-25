531659c8134b4728bcf4b9269d4ad80eac596904
vmzakharov <31494981+vmzakharov@users.noreply.github.com>
2024-08-25 18:54:48 -0400

    adds 1brc solution without logging

--- 1brc/pandasdf_1brc_with_logging.py
+++ 1brc/pandasdf_1brc_with_logging.py
@@ -1,8 +1,16 @@
 import pandas as pd
+import time
 
 FILE = "measurements_10.txt"
 
+def print_time(message, begin_time, end_time):
+    delta_ms = int((end_time - begin_time) * 1000)
+    print(f"{message}: {delta_ms} ms")
+
+
 if __name__ == "__main__":
+    start_time = time.monotonic()
+
     df = pd.read_csv(
         FILE,
         sep=";",
@@ -11,11 +19,19 @@ if __name__ == "__main__":
         engine='pyarrow'
     )
 
+    load_time = time.monotonic()
+
+    print_time("Loaded", start_time, load_time)
+
     df = df.groupby("Station").agg(["min", "mean", "max"])
 
     df.columns = df.columns.droplevel()
 
     df = df.sort_values("Station")
+
+    end_time = time.monotonic()
+
+    print_time("Aggregated", load_time, end_time)
 
     for index, row in df.iterrows():
         print(f"{index}={row['min']:0.1f}/{row['mean']:0.1f}/{row['max']:0.1f}")
